1ca186296b249ae59c8acbf959d54cd859b3f2c6
guo <503392549@qq.com>
2019-05-18 22:53:40 +0800

    内容更新

--- gulpfile.js
+++ gulpfile.js
@@ -7,49 +7,51 @@ const sass = require("gulp-sass");
 const csso = require("gulp-csso");
 const autoprefixer = require("gulp-autoprefixer")
 
-gulp.task("compileJS", ()=>{
+gulp.task("compileJS", () => {
 	gulp.src("src/scripts/**/*.js")
-		.pipe( babel({
-			presets : ["@babel/env"]
-		}) )
-		.pipe( uglify() )
-		.pipe( gulp.dest("dist/scripts") )
+		.pipe(babel({
+			presets: ["@babel/env"]
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest("dist/scripts"))
 	gulp.src("src/pages/**/*.js")
-		.pipe( babel({
-			presets : ["@babel/env"]
-		}) )
-		.pipe( uglify() )
-		.pipe( gulp.dest("dist/pages") )
-	gulp.src("src/static/**/*").pipe( gulp.dest("dist/static") );
+		.pipe(babel({
+			presets: ["@babel/env"]
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest("dist/pages"))
+	gulp.src("src/static/**/*").pipe(gulp.dest("dist/static"));
 })
-gulp.task("compileCSS", ()=>{
+gulp.task("compileCSS", () => {
 	gulp.src("src/styles/**/*.scss")
-		.pipe( sass() )
-		.pipe( autoprefixer() )
-		.pipe( csso() )
-		.pipe( gulp.dest("dist/styles") )
+		.pipe(sass())
+		.pipe(autoprefixer())
+		.pipe(csso())
+		.pipe(gulp.dest("dist/styles"))
 })
-gulp.task("compileHTML", ()=>{
+gulp.task("compileHTML", () => {
 	gulp.src("src/pages/**/*.html")
-		.pipe( gulp.dest("dist/pages") )
+		.pipe(gulp.dest("dist/pages"))
 })
 
 
-gulp.task("server", function(){
+gulp.task("server", function () {
 	//静态资源服务器 : 9999
 	gulp.src("dist")
-		.pipe( webserver({
-			livereload : true,
-			port : 9999
-		}) )
+		.pipe(webserver({
+			livereload: true,
+			port: 9999,
+			
+			//接口代理服务器
+			
+			
+		}))
 	gulp.watch("src/pages/**/*.js", ["compileJS"]);
 	gulp.watch("src/scripts/**/*.js", ["compileJS"]);
 	gulp.watch("src/styles/**/*.scss", ["compileCSS"]);
-	gulp.watch("src/pages/**/*.html", ["compileHTML"])
-	
-	//接口代理服务器
+	gulp.watch("src/pages/**/*.html", ["compileHTML"]);
 
 })
 
 
-gulp.task("build", ["compileJS","compileCSS","compileHTML"])
+gulp.task("build", ["compileJS", "compileCSS", "compileHTML"])
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,19 +1,180 @@
-require(["../../static/conf/config.js"], function(){
-	require(["jquery","sw"], function($, Swiper){
-		var mySwiper = new Swiper ('.swiper-container', {
-			direction: 'vertical', // 垂直切换选项
-			loop: true, // 循环模式选项
-			
-			// 如果需要分页器
-			pagination: {
-			  el: '.swiper-pagination',
+require(["../../static/conf/config.js"], function () {
+	require(["jquery", "sw"], function ($, Swiper) {
+		var jsonp53 ={};
+		var ych ={};
+		var category=[
+			{
+				href:"//search.damai.cn/search.htm?ctl=%E6%BC%94%E5%94%B1%E4%BC%9A&order=1&cty=",
+				item:"ych",
+				info:"演唱会"
 			},
-			
-			// 如果需要前进后退按钮
-			navigation: {
-			  nextEl: '.swiper-button-next',
-			  prevEl: '.swiper-button-prev',
+			{
+				href:"//search.damai.cn/search.htm?ctl=%E6%BC%94%E5%94%B1%E4%BC%9A&order=1&cty=",
+				item:"hjgj",
+				info:"话剧歌剧"
+			},
+			{
+				href:"//search.damai.cn/search.htm?ctl=%E6%BC%94%E5%94%B1%E4%BC%9A&order=1&cty=",
+				item:"tyjj",
+				info:"体育"
+			},
+			{
+				href:"//search.damai.cn/search.htm?ctl=%E6%BC%94%E5%94%B1%E4%BC%9A&order=1&cty=",
+				item:"qz",
+				info:"儿童亲子"
+			},
+			{
+				href:"//search.damai.cn/search.htm?ctl=%E6%BC%94%E5%94%B1%E4%BC%9A&order=1&cty=",
+				item:"zlxx",
+				info:"展览休闲"
+			},
+			{
+				href:"//search.damai.cn/search.htm?ctl=%E6%BC%94%E5%94%B1%E4%BC%9A&order=1&cty=",
+				item:"yyh",
+				info:"音乐会"
+			},
+			{
+				href:"//search.damai.cn/search.htm?ctl=%E6%BC%94%E5%94%B1%E4%BC%9A&order=1&cty=",
+				item:"xsxq",
+				info:"曲苑杂坛"
+			},
+			{
+				href:"//search.damai.cn/search.htm?ctl=%E6%BC%94%E5%94%B1%E4%BC%9A&order=1&cty=",
+				item:"wdbl",
+				info:"舞蹈芭蕾"
+			},
+			{
+				href:"//search.damai.cn/search.htm?ctl=%E6%BC%94%E5%94%B1%E4%BC%9A&order=1&cty=",
+				item:"dm",
+				info:"二次元"
+			},
+			{
+				href:"//search.damai.cn/search.htm?ctl=%E6%BC%94%E5%94%B1%E4%BC%9A&order=1&cty=",
+				item:"lyzl",
+				info:"旅游展览"
+			},
+		]
+		var restUrl= ["https://api-gw.damai.cn//search.html?cat=1&destCity=%E5%85%A8%E5%9B%BD&_ksTS=1558188816507_87",
+		"https://api-gw.damai.cn//search.html?cat=3&destCity=%E5%85%A8%E5%9B%BD&_ksTS=1558188816512_101",
+		"https://api-gw.damai.cn//search.html?cat=6&destCity=%E5%85%A8%E5%9B%BD&_ksTS=1558188816517_115",
+		"https://api-gw.damai.cn//search.html?cat=100&destCity=%E5%85%A8%E5%9B%BD&_ksTS=1558188816519_129"
+		]
+		var content_list=
+		
+		[{"name":"演唱会","id":"1"},{"name":"话剧歌剧","id":"3"},{"name":"体育比赛","id":"6","link":"https://p.damai.cn/wow/pc/act/sports"},{"name":"儿童亲子","id":"100"}]
+	
+		var tpl;
+			var mySwiper = new Swiper('.swiper-container', {
+				autoplay: true,//等同于以下设置
+				loop: true, // 循环模式选项
+				spaceBetween: 30,
+				effect: 'fade',
+				// 如果需要分页器
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true,
+				},
+				// 如果需要前进后退按钮
+				navigation: {
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev',
+				}
+			})
+			// 抓取城市
+			$.ajax({
+				url: `https://api-gw.damai.cn/cityList.html?_ksTS=1558163181541_52`,
+				dataType:'jsonp',
+				type: "get",
+				processData: false, 
+				crossDomain: true,
+				success: function (data) {
+				
+					for (const key in data.hotCities) {
+						 tpl = `<span class="name-city" data-spm="dselectcity&amp;clicktitle=${data.hotCities[key]}">${data.hotCities[key]}</span>`
+						$('.hot-city').children(".list-other").append(tpl);
+					}
+					for (const key in data.allCities) {
+						 tpl = `<span class="name-city" data-spm="dselectcity&amp;clicktitle=${data.allCities[key]}">${data.allCities[key]}</span>`
+						 $('.other-city').children(".list-other").append(tpl);
+					}
+					
+				},
+			})
+		$(".location-header").on('mousemove',function () {  
+			$(".city-header").show();
+		})
+		$(".location-header").on('mouseout',function () {  
+			$(".city-header").hide();
+		})
+			// 循环抓取首页
+			for (let i = 0; i < 4; i++) {
+				$.ajax({
+					url: restUrl[i],
+					dataType:'jsonp',
+					type: "get",
+					processData: false, 
+					crossDomain: true,
+					success: function (data) {
+						var $res = render_content(data);
+						$('.dm-content-wrap').append($res);
+					},
+				})
 			}
-		  })      
+			
+		
+		function render_category_item(index){
+			var tpl = `<a href="${category[index].href}" class="category__list" data-spm="ditem_${index}">
+						<span class="category__icon category__icon__${category[index].item}"></span>
+							<p class="category__info">
+							${category[index].info}
+								</p>
+						</a> `;
+						return tpl;
+		}
+		(function render_category(){
+			for (let i = 0; i < category.length; i++) {
+				var $item = render_category_item(i);
+				$('.category').append($item);
+				}
+		})()
+		function render_content_item(data,index){
+			if(index==0){
+				var left_box = data;
+				var tpl = `
+				<a href="#" class="box-left" target="_blank" data-spm="ditem_0"">
+				<img class="box-left__bg" src="${data.verticalPic}">
+				<div class="box-left__info">
+					<div class="title">${data.name}</div>
+			<div class="details">¥${data.formattedPriceStr.slice(0,data.formattedPriceStr.indexOf('-'))}<span>起</span></div>
+				</div>
+			</a>`;
+
+			}
+			var tpl = `<div class="dm-content dm-content-1" data-spm="card_${item}" style="display: block;">
+							<div class="head">
+								<span class="head-title">${title}</span>
+								<a href="//search.damai.cn/search.htm?ctl=%E6%BC%94%E5%94%B1%E4%BC%9A&amp;order=1" target="_blank" data-spm="dviewall">
+									<span class="head-more">查看全部</span>
+								</a>
+							</div>
+							<div class="box">
+							${left_box}
+								<div class="box-right">
+								${info_list}
+								</div>
+							</div>
+						</div>`;
+
+		}
+		function render_content(data) {
+			for (let i = 0; i < data.length; i++) {
+				var $item = render_category_item(data[i],i);
+			}
+				
+				var $result = 0;
+			return $result;
+		  }
+
+
 })
 })
